1e6da2bf9400f55f55a63f9bf3a211712888b0ce
LirimMuqolli <57811400+LirimMuqolli@users.noreply.github.com>
2023-01-31 22:07:30 +0100

    create header footer

---
+++ keeperapp/src/components/App.js
@@ -0,0 +1,14 @@
+import Header from './Header';
+import '../../public/styles.css'
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div>
+    <Header />
+    <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ keeperapp/src/components/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from '../components/App';
+
+
+
+ReactDOM.render(<div>
+  <App />
+</div>,document.getElementById("root"));
