d3fa37b2f0c9231268bb3d3b98948f582d338255
LirimMuqolli <57811400+LirimMuqolli@users.noreply.github.com>
2023-02-03 20:31:44 +0100

    Keeper app Part 2

--- keeperapp/src/components/App.js
+++ keeperapp/src/components/App.js
@@ -1,15 +1,21 @@
-import Header from './Header';
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
 
-import Footer from './Footer';
-import Note from './Note';
 function App() {
   return (
     <div>
-    <Header />
-
-    <Note />
-
-    <Footer />
+      <Header />
+      {notes.map(noteItem => (
+        <Note
+          key={noteItem.key}
+          title={noteItem.title}
+          content={noteItem.content}
+        />
+      ))}
+      <Footer />
     </div>
   );
 }
